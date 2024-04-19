971ae5567366b1f8a15a8cf3890f16c655781468
Jonathan Andueza <jonathanandueza1201@gmail.com>
2024-04-18 21:51:00 -0300

    Styles fixes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,50 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    renderChartData(data.gender, 'chart');
-    renderChartData(data.ages, 'chart2');
-  })
-  .catch(error => {
-    console.error('Error fetching or processing data:', error);
-  });
+    const genderData = data.gender;
+    const chartContainer = document.getElementById('chart');
 
-function renderChartData(data, chartId) {
-  const chartContainer = document.getElementById(chartId);
-  
-  Object.entries(data).forEach(([label, { percentage }]) => {
-    const barContainer = document.createElement('div');
-    barContainer.classList.add('bar-container');
-    
-    const bar = document.createElement('div');
-    bar.classList.add('bar');
-    bar.style.width = `${percentage}%`;
-    
-    const percentageDiv = document.createElement('div');
-    percentageDiv.classList.add('percentage-label');
-    percentageDiv.textContent = `${percentage}%`;
-    
-    const labelDiv = document.createElement('div');
-    labelDiv.classList.add('gender-label');
-    labelDiv.textContent = label;
-    
-    barContainer.appendChild(percentageDiv);
-    barContainer.appendChild(labelDiv);
-    barContainer.appendChild(bar);
-    
-    chartContainer.appendChild(barContainer);
+    Object.entries(genderData).forEach(([gender, { percentage }]) => {
+      const barContainer = document.createElement('div');
+      barContainer.classList.add('bar-container');
+      const bar = document.createElement('div');
+      bar.classList.add('bar');
+      bar.style.width = `${percentage}%`;
+
+      const percentageDiv = document.createElement("div");
+      percentageDiv.classList.add("percentage-label");
+      percentageDiv.textContent = percentage + "%";
+      barContainer.appendChild(percentageDiv); 
+
+      const genderLabel = document.createElement("div");
+      genderLabel.classList.add("gender-label");
+      genderLabel.textContent = gender; 
+      barContainer.appendChild(genderLabel); 
+
+      barContainer.appendChild(bar);
+      chartContainer.appendChild(barContainer);
+    });
+
+    const agesData = data.ages;
+    const chartContainer2 = document.getElementById('chart2');
+
+    Object.entries(agesData).forEach(([age, { percentage }]) => {
+      const barContainer = document.createElement('div');
+      barContainer.classList.add('bar-container');
+      const bar = document.createElement('div');
+      bar.classList.add('bar');
+      bar.style.width = `${percentage}%`;
+
+      const percentageDiv = document.createElement("div");
+      percentageDiv.classList.add("percentage-label");
+      percentageDiv.textContent = percentage + "%";
+      barContainer.appendChild(percentageDiv);
+
+      const ageLabel = document.createElement("div");
+      ageLabel.classList.add("gender-label");
+      ageLabel.textContent = age + " years";
+      barContainer.appendChild(ageLabel);
+      barContainer.appendChild(bar);
+      chartContainer2.appendChild(barContainer);
+    });
   });
-}
